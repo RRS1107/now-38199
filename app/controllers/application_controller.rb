946b36caa92b802823a256b49ebb97166f242d16
RRS1107 <thh0508@gmail.com>
2022-07-26 23:10:22 +0900

    deviseの導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :staff_code, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:staff_code, :password ])
+  end
 end
